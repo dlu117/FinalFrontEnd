5b6bc7f7a0da70fcb3ac0c281297afb3c3ce324d
Daphne <dsmhan7@gmail.com>
2019-08-01 02:06:55 +1200

    adding bookshelf component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,83 @@
 import * as React from 'react';
+import BookList from 'src/Components/BookList';
+import ReactPlayer from 'react-player';
 
 
-
-class App extends React.Component {
-  public render() {
-    return (
-     
-     
-      <div/>
-    );
-  }
+interface IState {
+ // displayer: any,
+  updateBookList: any,
+  displayURL: string
+  // videoList: object
 }
 
+
+class App extends React.Component<{}, IState>{
+  public constructor(props: any) {
+    super(props);
+    this.state = {
+      // displayer: null,
+      displayURL: "",   // Initial no book displayed
+      updateBookList: null
+     // videoList: []
+    }
+  }
+
+
+  public addVideo = (url: string) => {
+    const body = {"url": url}
+    fetch("https://bookapidevops.azurewebsites.net/api/Books", {
+      body: JSON.stringify(body),
+      headers: {
+        Accept: "text/plain",
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    }).then(() => {
+       this.state.updateBookList();   // will call function within BookList
+    })
+  }
+
+  public listMounted = (callbacks: any) => {
+    this.setState({ updateBookList: callbacks })
+  }
+
+  public updateURL = (url: string) => {   // to change book we are displaying
+    if(this.state.displayURL === url){    // check if its already displayed
+      this.setState({displayURL : ""},() => this.setState({displayURL: url}))
+    }else{
+      this.setState({displayURL:url})
+    }
+  }
+  public render() {
+    return (<div>
+    
+      <div className="container">
+        <div className="row">
+          <div className="col-7">
+            <ReactPlayer
+              className="player"
+              controls={true}
+              url={this.state.displayURL}
+              width="100%"
+              height="400px"
+              playing={true}
+              config={{
+                youtube: {
+                  playerVars: { showinfo: 1 },
+                  preload: true
+                }
+              }
+              }
+            />
+          </div>
+          <div className="col-5">
+          <BookList display={this.updateURL} mount={this.listMounted} />
+          </div>
+        </div>
+        
+      </div>
+    </div>)
+  }
+ 
+}
 export default App;
---
+++ src/Components/BookList.tsx
@@ -0,0 +1,88 @@
+import Tick from '@material-ui/icons/CheckCircle'
+import TickBorder from '@material-ui/icons/CheckCircleOutlineOutlined'
+import Close from '@material-ui/icons/Close'
+import * as React from 'react'
+
+interface IState{
+    bookList:any
+}
+
+interface IProps{
+    display:any,   
+    mount:any,
+}
+
+export default class BookList extends React.Component<IProps,IState>{
+    public constructor(props:any){
+        super(props);
+        this.state = {   // object takes empty array 
+            bookList: [],
+        }
+        this.updateList();
+    } 
+
+    // App component will have access to the function so we can call it within app.tsx
+    public componentDidMount = () =>{
+        this.props.mount(this.updateList)
+    }
+
+    public updateList = () => {
+        fetch('https://bookapidevops.azurewebsites.net/api/Books',{
+            method:'GET'  // get the book objects stored in database
+        }).then((response:any) => {
+            return response.json();  // convert into JSON format
+        }).then((response:any)=>{
+            const output:any[] = []
+            response.forEach((book:any) => {  
+                const row = (<tr> 
+                    <td className="align-middle" onClick={() => this.handleRead(book)}>{book.isRead === true?< Tick />:<TickBorder/>}</td>  
+                    <td className="align-middle" onClick={() => this.props.display(book.thumbnailUrl)}><img src={book.thumbnailUrl} width="80px"/></td>
+                    <td className="align-middle" onClick={() => this.deleteBook(book.bookId)}><Close/></td>                    
+                    </tr>)
+                if(book.isRead){
+                    output.push(row); // read books are at the bottom of the list 
+                }else{               
+                    output.unshift(row)  // adds item to start of list
+                }
+            })
+            this.setState({bookList:output})
+            });
+    }
+
+    public deleteBook = (id:any) => {
+        fetch("https://bookapidevops.azurewebsites.net/api/Books/"+id,{
+            method:"DELETE"
+        }).then(()=>{
+            this.updateList() // once delete list update list again
+        })
+    }
+
+    public handleRead = (book:any) =>{
+        const toSend = [{
+            "from":"",
+            "op":"replace",
+            "path":"/isRead",
+            "value":!book.isRead, // ! will flip value
+        }]
+        fetch("https://bookapidevops.azurewebsites.net/api/Books/update/"+book.bookId,{
+            body:JSON.stringify(toSend),
+            headers: {
+                Accept: "text/plain",
+                "Content-Type":"application/json-patch+json"
+            },
+            method:"PATCH" 
+        }).then(()=>{this.updateList()})
+    }
+
+   
+    public render() {  
+        return (
+            <div className="booklist">  
+            <h1 className="books-heading">Book Shelf</h1>
+            <table className="table">  
+                {this.state.bookList}
+            </table> 
+            </div>
+        )
+    }
+}
